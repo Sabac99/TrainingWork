bab1d4f4845f91751a85ae7b2cc656fb942ccfa7
Sabac99 <kalbosaVkysnaya06@gmail.com>
2025-10-18 15:03:33 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Random random = new Random();
+int[] numbers = new int[random.Next(1, 100)];
+String output;
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = random.Next(10, 20);
+}
+output = string.Join(" ", numbers);
+Console.WriteLine($"{output}, Длина = {numbers.Length}");
