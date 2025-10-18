302fd158a7f5633c74e42c81c5ab284b3fd71381
Sabac99 <kalbosaVkysnaya06@gmail.com>
2025-10-18 22:31:06 +0700

    2 Task completed

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿Random random = new Random();
+﻿using System.Text;
+//1
+
+
+
+Random random = new Random();
 int[] numbers = new int[random.Next(1, 100)];
 String output;
 for (int i = 0; i < numbers.Length; i++)
@@ -7,3 +12,54 @@ for (int i = 0; i < numbers.Length; i++)
 }
 output = string.Join(" ", numbers);
 Console.WriteLine($"{output}, Длина = {numbers.Length}");
+Console.WriteLine("\n");
+
+
+
+//2
+int x = random.Next(1,10);
+int y = random.Next(1,10);
+int[,] lotOfNumbers = new int[x, y]; 
+for  (int i = 0; i < x; i++)
+{
+    for (int j = 0; j < y; j++)
+    {
+        lotOfNumbers[i, j] = random.Next(1, 100);
+    }
+}
+int FindMax(int[,] array) {
+    int max = -1;
+    x = lotOfNumbers.GetUpperBound(0) + 1;
+    y = lotOfNumbers.GetUpperBound(1) + 1;
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            if (lotOfNumbers[i, j] > max)
+            {
+                max = lotOfNumbers[i, j];
+            }
+        }
+    }
+    return max;
+}
+
+void printArray(int[,] array)
+{
+    StringBuilder output = new StringBuilder();
+    x = lotOfNumbers.GetUpperBound(0) + 1;
+    y = lotOfNumbers.GetUpperBound(1) + 1;
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            output.Append(array[i, j].ToString());
+            output.Append(" ");
+        }
+        output.Append('\n');
+    }
+    Console.WriteLine(output);
+}
+printArray(lotOfNumbers);
+Console.WriteLine($"Максимум = {FindMax(lotOfNumbers)}");
+
